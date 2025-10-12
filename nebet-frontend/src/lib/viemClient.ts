bc9c3407a55d8cc5ef9390bdeaf69f77fc485afa
Alvaro Lamadrid <aglamadrid19@gmail.com>
2025-10-12 12:51:37 -0600

    cleaning up contract repo

--- nebet-frontend/src/lib/viemClient.ts
+++
@@ -1,7 +0,0 @@
-import { createPublicClient, http } from "viem";
-import { NETWORK_CHAIN, RPC_URL } from "../config";
-
-export const publicClient = createPublicClient({
-  chain: NETWORK_CHAIN,
-  transport: http(RPC_URL)
-});
